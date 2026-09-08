391abec950a32818eaa29f8d7b7325d894afd262
atharv <atharvdeshmukh501@gmail.com>
2026-09-08 19:06:49 +0530

    Add two new methods in cache class

--- src/main/java/com/example/demo/cache/Cache.java
+++ src/main/java/com/example/demo/cache/Cache.java
@@ -1,5 +1,18 @@
 package com.example.demo.cache;
 
-public class Cache {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.example.demo.entity.Weather;
+
+public class Cache {
+	List<Weather> l = new ArrayList<>();
+
+	public Weather getData(int pin) {
+		return l.get(pin);
+	}
+
+	public void saveData(Weather w) {
+		l.add(w);
+	}
 }
